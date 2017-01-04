e08cac7b0887cb0dfc0976994cc048e468a1c255
adithya <jaldaadithya@gmail.com>
2017-01-04 17:15:09 +0530

    edited

--- hello-world-git/src/com/explore/git/options/GitHelloWorld.java
+++ hello-world-git/src/com/explore/git/options/GitHelloWorld.java
@@ -5,7 +5,7 @@ public class GitHelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, welcome to git..........!!!");
 		System.out.println("Committing code from user2");
-		
+		System.out.println("Committing code from user3");
 	}
 
 }
