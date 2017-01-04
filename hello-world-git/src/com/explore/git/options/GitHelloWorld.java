fc99c96bf75b8ea6c4cd9a62b8f4b1a74d5a5870
Raja Shekhar <rajashekharemail2@gmail.com>
2017-01-04 17:02:47 +0530

    commit from user2

--- hello-world-git/src/com/explore/git/options/GitHelloWorld.java
+++ hello-world-git/src/com/explore/git/options/GitHelloWorld.java
@@ -4,6 +4,8 @@ public class GitHelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, welcome to git..........!!!");
+		System.out.println("Committing code from user2");
+		
 	}
 
 }
